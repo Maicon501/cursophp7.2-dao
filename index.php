02fa47dd8035c7728ce6c04637cdd2cac55567d0
Maicon501 <maicondmonteiro@gmail.com>
2024-04-16 20:29:42 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -1,25 +1,38 @@
 <?php
 
 require_once("config.php");
-//carrega um usuario
-//$danielle = new Usuario();
-//$danielle->loadbyId(5);
-//echo $danielle;
-//$sql = new sql();
-//$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-//echo json_encode($usuarios);
-
-//carrega uma lista de usuarios
-//$lista = Usuario::getList();
-//echo json_encode($lista);
-
+/*carrega um usuario
+$danielle = new Usuario();
+$danielle->loadbyId(5);
+echo $danielle;
+$sql = new sql();
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+echo json_encode($usuarios);
+*/
+/*carrega uma lista de usuarios
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
 //carrega uma lista de usuarios buscando pelo login
 //$search = Usuario::search("fla");
 //echo json_encode($search);
 
 //carrega um usuario usando o login e a senha
-$usuario = new Usuario();
-$usuario->login("danielle", "270898");
+//$usuario = new Usuario();
+//$usuario->login("danielle", "270898");
+//echo $usuario;
+/*
+//criando um novo usuario
+$aluno = new Usuario();
+$aluno->setDeslogin("aluno");
+$aluno->setDessenha("aluno");
+$aluno->insert();
+echo $aluno;
+*/
 
+$usuario = new Usuario();
+$usuario->loadById(5);
+$usuario->UpDate("professor", "654321");
 echo $usuario;
+
 ?>
